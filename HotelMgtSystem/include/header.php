c3bb5ec109ceadf431a0b4a0dec010d1b0997f44
Sanjani Gunathilaka <pmts12344321@gmail.com>
2017-08-03 20:04:31 +0530

    Add files via upload

---
+++ HotelMgtSystem/include/header.php
@@ -0,0 +1,60 @@
+<?php
+    session_start();    
+?>
+
+<html>
+    <head>
+        <title>Hotel Management System</title>
+        <link href="../css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div>
+            <table>
+                <tr>
+                    <td><img id="userlogo" src="../images/user.jpg"></td>
+                    <td>LasiniCL</td>
+                </tr>
+            </table>
+        </div>
+
+        <ul>
+            <li><a>Rooms</a>
+                <ul>
+                    <li><a>Reserve Room</a></li>
+                    <li><a>Update Reservation</a></li>
+                    <li><a>Delete Reservation</a></li>
+                </ul>
+            </li>
+            <li><a>Events</a>
+                <ul>
+                    <li><a>Add Event</a></li>
+                    <li><a>Update Event</a></li>
+                    <li><a>Delete Event</a></li>
+                </ul>
+            </li>
+            <li><a>Restaurant</a>
+                <ul>
+                    <li><a href="../restaurant/MakeOrder.php">Make Order</a></li>
+                    <li><a href="../restaurant/UpdateOrder.php">Update Order</a></li>
+                    <li><a href="../restaurant/DeleteOrder.php">Delete Order</a></li>
+                </ul>
+            </li>
+            <li><a>Stock</a>
+                <ul>
+                    <li><a>Add Item</a></li>
+                    <li><a>Update Item</a></li>
+                    <li><a>Delete Item</a></li>
+                </ul>
+            </li>
+            <li><a>Employees</a>
+                <ul>
+                    <li><a>Add Employee</a></li>
+                    <li><a>Update Employee</a></li>
+                    <li><a>Delete Employee</a></li>
+                </ul>
+            </li>
+            <li></li>
+        </ul>
+
+    </body>
+</html>
